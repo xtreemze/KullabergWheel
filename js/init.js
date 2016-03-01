1766aee0f17c7393de45aba04f0a12c03bf9f362
xtreemze <carlos@2activedesign.com>
2016-03-01 15:34:55 +0100

    copying from webserver

---
+++ js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+  $('.button-collapse').sideNav({
+      menuWidth: 250, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
+  $('select').material_select();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
