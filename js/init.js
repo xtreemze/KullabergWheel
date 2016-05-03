8e86bb0608dc3b7c40fe1fbba654ca438824d73b
Carlos Velasco <carlos@2activedesign.com>
2016-05-03 17:36:40 +0200

    Minify Files

--- js/init.js
+++ js/init.js
@@ -1,12 +1,11 @@
-(function($){
-  $(function(){
+(function ($) {
+	$(function () {
 
-  $('.button-collapse').sideNav({
-      menuWidth: 250, // Default is 240
-      edge: 'left', // Choose the horizontal origin
-      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    }
-  );
-  $('select').material_select();
-  }); // end of document ready
+		$('.button-collapse').sideNav({
+			menuWidth: 250, // Default is 240
+			edge: 'left', // Choose the horizontal origin
+			closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+		});
+		$('select').material_select();
+	}); // end of document ready
 })(jQuery); // end of jQuery name space
