723dd57ae312c3584c188980f3dbe5b9293b88e9
Carlos Velasco <carlos@2activedesign.com>
2016-05-13 10:33:21 +0200

    Standardize

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+// !minifyOnSave
+// minifyOnSave: false
+// !minOnSave
+// minOnSave: false
+module.exports = {
+  // "ecmaFeatures": {
+  // 	"jsx": true,
+  // 	"modules": true,
+  // 	"arrowFunctions": true,
+  // 	"classes": true,
+  // 	"spread": true,
+  // },
+  "env": {
+    "browser": true,
+    // "es6": true
+  },
+  "extends": "airbnb",
+  // "rules": {
+  //   	"indent": [ "error", "tab" ],
+  //   "linebreak-style": ["error", "windows"],
+  //    	"quotes": [ "error", "single" ],
+  //    	"semi": [ "error", "always" ]
+  // },
+  "globals": {
+    "$": true,
+    "Materialize": true,
+    "jQuery": true,
+    "intervalID": true
+  }
+};
--- js/init.js
+++ js/init.js
@@ -1,11 +1,10 @@
-(function ($) {
-	$(function () {
-
-		$('.button-collapse').sideNav({
-			menuWidth: 250, // Default is 240
-			edge: 'left', // Choose the horizontal origin
-			closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-		});
-		$('select').material_select();
-	}); // end of document ready
-})(jQuery); // end of jQuery name space
+(function jQ($) {
+  $(function docReady() {
+    $('.button-collapse').sideNav({
+      menuWidth: 250, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    });
+    $('select').material_select();
+  }); // end of document ready
+}(jQuery)); // end of jQuery name space
